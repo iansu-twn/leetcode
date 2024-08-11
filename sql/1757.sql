b4d9e418feabe87489a3727c7972d46e7beed2d4
iansu-twn <iansu.job@gmail.com>
2024-08-12 01:36:39 +0800

    add another solution

--- sql/1757.sql
+++ sql/1757.sql
@@ -6,9 +6,18 @@ and recyclable = 'y'
 def find_products(
     product: pd.DataFrame
 ) -> pd.DataFrame:
+    
+    -- method 1
     products = products.loc[
         lambda row:
             (row["low_fats"] == 'Y') &
             (row["recyclable"] == 'Y')
     ][["product_id"]]
     return products
+
+    -- method 2
+    res = products[
+        (products["low_fats"] == 'Y') &
+        (products["recyclable"] == 'Y')
+    ][["product_id"]]
+    return res
