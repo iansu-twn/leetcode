9bff5e64095494dc54ccedbbd56d42e21a16ccb7
ian <iansu.job@gmail.com>
2024-09-19 12:01:40 +0000

    add different pandas solution

--- sql/1661.sql
+++ sql/1661.sql
@@ -17,6 +17,7 @@ and b.activity_type = 'end'
 group by a.machine_id
 
 def get_average_time(activity: pd.DataFrame) -> pd.DataFrame:
+    -- method 1
     df = activity.merge(
         activity,
         how="inner",
@@ -30,3 +31,25 @@ def get_average_time(activity: pd.DataFrame) -> pd.DataFrame:
     df["dif"] = df["timestamp_b"] - df["timestamp_a"]
     df = df.groupby(["machine_id"], as_index=False).agg({"dif": np.average}).round(3)
     return df.rename(columns={"dif": "processing_time"})
+
+    -- method 2
+    df = activity.pivot(
+        index=["machine_id", "process_id"],
+        columns="activity_type",
+        values="timestamp"
+    )
+    df["processing_time"] = df["end"] - df["start"]
+    df = df.groupby(["machine_id"]).agg({"processing_time": "mean"}).round(3).reset_index()
+    return df
+
+    -- method 3
+    return (activity.pivot(
+        index=["machine_id", "process_id"],
+        columns="activity_type",
+        values="timestamp"
+    )
+    .groupby(["machine_id"])
+    .apply(lambda row: (row["end"] - row["start"]).mean().round(3))
+    .rename("processing_time")
+    .reset_index()
+    )
